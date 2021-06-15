a83e19343cec7c5e57634933217144668fa3c855
Gwen <dryystan@outlook.fr>
2021-06-15 11:50:06 +0200

    commit initial

---
+++ src/test/java/com/gwendal/magasinJPA/MagasinJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwendal.magasinJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagasinJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
